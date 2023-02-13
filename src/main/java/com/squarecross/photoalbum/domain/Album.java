e58baaf0253989198e9e513b44a2970028a511c2
eden <haerim2429@gmai.com>
2023-02-13 23:54:18 +0900

    photo domain 추가, Photo_FK설정, Album_FK설정

--- src/main/java/com/squarecross/photoalbum/domain/Album.java
+++ src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name= "album",schema= "photo_album", uniqueConstraints ={@UniqueConstraint(columnNames = "album_id")})
@@ -14,13 +15,15 @@ public class Album {
     @Column(name="album_id", unique = true, nullable = false)
     private Long albumId;
 
-    @Column(name="albume_name" , unique = false, nullable = false)
+    @Column(name="album_name" , unique = false, nullable = false)
     private String albumName;
 
     @Column(name="created_at", unique = false, nullable = true)
     @CreationTimestamp
     private Date createdAt;
 
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="album")
+    private List<Photo> photos;
 
     public Album(){};
 
